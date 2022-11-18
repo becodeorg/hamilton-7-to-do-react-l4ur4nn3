7516699420abe5fbc05ca78aec1c5bcf8fde170a
l4ur4nn3 <nicolaslauranne99@gmail.com>
2022-11-18 12:14:19 +0100

    push before disaster

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import './App.css';
+import Button from './components/button'
 
 function App() {
   return (
-    <div className="App">
-        <h1>
+    <div class="App bg-cyan-800 text-left pt-10 pb-10">
+        <h1 class="ml-10 mb-10 text-2xl">
           My Todo App
         </h1>
-        <input type="text" placeholder='Type a new todo'>
+        <input type="text" placeholder='Type a new todo' class="ml-10 placeholder:pl-3 placeholder:text-sm">
         </input>
-        <input type="submit" value="Add todo">
-        </input>
-        <h2>
+        <Button />
+        <hr />
+        <h2 class="ml-10 mt-10 mb-10 text-xl">
           Todos
         </h2>
-        <input type="checkbox" id="checkbox1" name="checkbox1">
+        <input type="checkbox" id="checkbox1" name="checkbox1" class="ml-10">
         </input>
-        <label for="checkbox1">Learn React</label><br>
-        <input type="checkbox" id="checkbox1" name="checkbox1">
+        <label for="checkbox1" class="ml-5 text-base">Learn React</label><br />
+        <input type="checkbox" id="checkbox1" name="checkbox1" class="ml-10">
         </input>
-        <label for="checkbox1">Be Awesome!</label>
+        <label for="checkbox1" class="ml-5 text-base">Be Awesome!</label>
     </div>
-  );
-}
+    );
+};
 
 export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,5 @@
+export default function Button() {
+    return ( 
+        <button class="ml-5 text-base">Add task</button>
+        )
+    }
